200eac71eca62e46c0a7afb8935cdbd189677268
mrodriguezoi <mrodriguezoi@outlook.es>
2024-04-21 21:44:55 -0300

    init

---
+++ src/schema/articleSchemas.ts
@@ -0,0 +1,46 @@
+export interface Article {
+  title: string;
+  content: ArticleContent;
+  slug: string;
+}
+
+interface ArticleContent {
+  sidebar: SidebarContent;
+  mainContentArray: Array<MainContent>;
+}
+
+interface SidebarContent {
+  imageUrl: string;
+  characterDetails?: CharacterOptions;
+  eventDetails?: EventOptions;
+  geographyDetails?: GeographyOptions;
+}
+
+export interface CharacterOptions {
+  status: string;
+  alignment: string;
+  race: string;
+  relationships?: Array<RelationshipOptions>;
+}
+
+interface RelationshipOptions {
+  type: string;
+  with: string;
+}
+
+export interface EventOptions {
+  type: string;
+  location: string;
+  impact: string;
+  date: string;
+}
+
+export interface GeographyOptions {
+  type: string;
+  location: string;
+}
+
+export interface MainContent {
+  title?: string;
+  text: string;
+}
